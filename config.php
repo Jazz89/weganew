c368a783bb6aa64c9d42545c43c2f21f5e3df6e5
Test Test <Test Test>
2017-04-07 16:32:20 +0200

    added gitignore

--- config.php
+++ config.php
@@ -6,23 +6,23 @@ define('HTTP_SERVER', 'http://silverwega.xaa.pl/newwega/');
 define('HTTPS_SERVER', 'http://silverwega.xaa.pl/newwega/');
 
 // DIR
-define('DIR_APPLICATION', '/home/silverwe/public_html/newwega/catalog/');
-define('DIR_SYSTEM', '/home/silverwe/public_html/newwega/system/');
-define('DIR_IMAGE', '/home/silverwe/public_html/newwega/image/');
-define('DIR_LANGUAGE', '/home/silverwe/public_html/newwega/catalog/language/');
-define('DIR_TEMPLATE', '/home/silverwe/public_html/newwega/catalog/view/theme/');
-define('DIR_CONFIG', '/home/silverwe/public_html/newwega/system/config/');
-define('DIR_CACHE', '/home/silverwe/public_html/newwega/system/storage/cache/');
-define('DIR_DOWNLOAD', '/home/silverwe/public_html/newwega/system/storage/download/');
-define('DIR_LOGS', '/home/silverwe/public_html/newwega/system/storage/logs/');
-define('DIR_MODIFICATION', '/home/silverwe/public_html/newwega/system/storage/modification/');
-define('DIR_UPLOAD', '/home/silverwe/public_html/newwega/system/storage/upload/');
+define('DIR_APPLICATION', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/catalog/');
+define('DIR_SYSTEM', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/');
+define('DIR_IMAGE', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/image/');
+define('DIR_LANGUAGE', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/catalog/language/');
+define('DIR_TEMPLATE', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/config/');
+define('DIR_CACHE', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/storage/download/');
+define('DIR_LOGS', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/Moje_SerweryWWW/wamp64/www/wegasrebro/weganew/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'silverwe_admin2');
-define('DB_PASSWORD', 'admin');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 define('DB_DATABASE', 'silverwe_new');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
